2cffb9ec4b5650882ef1d5cfd6caceb5c4db47b8
dav <davosim577@gmail.com>
2022-08-12 15:52:06 +0400

    Maven

---
+++ src/main/java/command/Commands.java
@@ -0,0 +1,44 @@
+package command;
+
+public interface Commands {
+
+    int LOGOUT = 0;
+    int ADD_STUDENTS = 1;
+    int ADD_PRINT_ALL_STUDENTS = 2;
+    int DELETE_STUDENT_BY_INDEX = 3;
+    int PRINT_STUDENT_BY_INDEX = 4;
+    int PRINT_STUDENT_COUNT = 5;
+    int CHANGE_STUDENT_LESSON = 6;
+    int ADD_LESSON = 7;
+    int PRINT_ALL_LESSON = 8;
+
+    int EXIT = 0;
+    int LOGIN = 1;
+    int REGISTER = 2;
+
+    static void printAdminCommands() {
+        System.out.println("please input " + LOGOUT + " for logout");
+        System.out.println("please input " + ADD_STUDENTS + " for add student");
+        System.out.println("please input " + ADD_PRINT_ALL_STUDENTS + " for print all student");
+        System.out.println("please input " + DELETE_STUDENT_BY_INDEX + " for delete student by index");
+        System.out.println("please input " + PRINT_STUDENT_BY_INDEX + " for print student by lesson");
+        System.out.println("please input " + PRINT_STUDENT_COUNT + " for print students count");
+        System.out.println("please choose " + CHANGE_STUDENT_LESSON + " for change student's lesson");
+        System.out.println("please choose " + ADD_LESSON + " for add lesson");
+        System.out.println("please choose " + PRINT_ALL_LESSON + " for print all lesson");
+    }
+
+    static void printLoginCommands() {
+        System.out.println("please input " + EXIT + " exit");
+        System.out.println("please input " + LOGIN + " for login");
+        System.out.println("please input " + REGISTER + " for register");
+    }
+    static void printUserCommands() {
+        System.out.println("please input " + LOGOUT + " for logout");
+        System.out.println("please input " + ADD_STUDENTS + " for add student");
+        System.out.println("please input " + ADD_PRINT_ALL_STUDENTS + " for print all student");
+        System.out.println("please input " + PRINT_STUDENT_COUNT + " for print students count");
+        System.out.println("please choose " + CHANGE_STUDENT_LESSON + " for change student's lesson");
+        System.out.println("please choose " + PRINT_ALL_LESSON + " for print all lesson");
+    }
+}
---
+++ src/main/java/storage/LessonStorage.java
@@ -0,0 +1,63 @@
+package storage;
+
+import model.Lesson;
+
+import java.util.Scanner;
+
+public class LessonStorage {
+    private static Scanner scanner = new Scanner(System.in);
+
+    private Lesson[] array = new Lesson[10];
+    private int size = -1;
+
+
+    public void add(Lesson lesson) {
+        if (size == array.length) {
+            extend();
+        }
+        array[++size] = lesson;
+    }
+
+
+    public void print() {
+        for (int i = 0; i < size + 1; i++) {
+            System.out.println(i + ". " + array[i]);
+        }
+    }
+
+
+    private void extend() {
+        if (size == array.length) {
+            Lesson[] temp = new Lesson[array.length + 10];
+            for (int i = 0; i < size; i++) {
+                temp[i] = array[i];
+            }
+            array = temp;
+        }
+    }
+
+    public void delete(int index) {
+        if (index < 0 || index > size) {
+            System.out.println("invalid index");
+        } else {
+            for (int i = index; i < size; i++) {
+                array[i] = array[i + 1];
+            }
+            size--;
+        }
+    }
+
+
+    public int getSize() {
+        return size + 1;
+    }
+
+    public Lesson getLessonByIndex(int indexName) {
+        if (indexName < 0 || indexName >= size) {
+            return null;
+        }
+        return array[indexName];
+    }
+}
+
+
